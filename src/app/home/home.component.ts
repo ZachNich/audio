0a71bcdc6a01be2fc2ca5e741f06f63cfaa40790
ZachNich <raiste.doctow@gmail.com>
2025-07-13 15:22:00 -0600

    add basic equalizer visual

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,92 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent implements OnInit {
+  @ViewChild('equalizerCanvas')
+  canvasRef?: ElementRef<HTMLCanvasElement>;
+  public isRecording = false;
+  public animationId: number | null = null;
+  public audioContext = new AudioContext();
+  public analyser: AnalyserNode | null = null;
+  public data: Uint8Array<ArrayBuffer> = new Uint8Array();
+
+  public ngOnInit(): void {
+    this.getMediaDevices();
+  }
+
+  public async getMediaDevices(): Promise<void> {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    console.log('devices: ', devices);
+  }
+
+  public async recordAudio(): Promise<void> {
+    if (this.isRecording) return;
+
+    this.isRecording = true;
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: false,
+      });
+      this.audioContext.state === 'suspended' &&
+        (await this.audioContext.resume());
+      const source = this.audioContext.createMediaStreamSource(stream);
+      this.analyser = this.audioContext.createAnalyser();
+      this.analyser.fftSize = 2048; // even num between 32 and 32768, higher = more detail/resolution/CPU usage
+      const bufferLength = this.analyser.frequencyBinCount;
+      this.data = new Uint8Array(bufferLength);
+      source.connect(this.analyser);
+
+      this.draw();
+    } catch (error) {
+      this.isRecording = false;
+    }
+  }
+
+  public async stopRecordingAudio(): Promise<void> {
+    try {
+      await this.audioContext.suspend();
+      this.animationId && cancelAnimationFrame(this.animationId);
+      this.isRecording = false;
+    } catch (error) {
+      //handle error, set recording state
+    }
+  }
+
+  private drawCallback = () => this.draw(); //handling callback this binding
+
+  private draw(): void {
+    if (!this.analyser) return;
+    if (!this.canvasRef) return;
+
+    const canvas = this.canvasRef.nativeElement;
+    const context = canvas.getContext('2d');
+    if (!context) return;
+
+    this.animationId = requestAnimationFrame(this.drawCallback);
+    this.analyser.getByteFrequencyData(this.data);
+
+    const { width, height } = canvas;
+    context.clearRect(0, 0, width, height);
+
+    //https://developer.mozilla.org/en-US/docs/Web/API/AnalyserNode/getByteFrequencyData
+    const barWidth = (width / this.data.length) * 2.5;
+    let x = 0;
+
+    for (const byte of this.data) {
+      const barHeight = byte / 2;
+      const red = byte;
+      const green = 255 - red;
+
+      context.fillStyle = `rgb(${red}, ${green}, 50)`;
+      context.fillRect(x, height - barHeight, barWidth, barHeight);
+
+      x += barWidth + 1;
+    }
+  }
+}
